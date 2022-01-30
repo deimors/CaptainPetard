d688b67275fe47f1dd0a8de40c18d356023fca05
deimors <deimors@gmail.com>
2022-01-30 11:33:00 -0600

    Enemies respawn at fixed spawn points

--- Assets/Code/Presentation/EnemySpawnPoint.cs
+++ Assets/Code/Presentation/EnemySpawnPoint.cs
@@ -1,3 +1,4 @@
+using System;
 using UniRx;
 using UnityEngine;
 using Zenject;
@@ -5,14 +6,27 @@ using Zenject;
 public class EnemySpawnPoint : MonoBehaviour
 {
 	public PlayerColours Colour;
+	public float RespawnDelaySeconds = 2;
 
 	[Inject]
 	public IEnemiesCommands EnemiesCommands { private get; set; }
 
+	[Inject]
+	public IEnemiesEvents EnemiesEvents { private get; set; }
+
 	void Start()
 	{
 		Observable.NextFrame()
-			.Subscribe(_ => EnemiesCommands.AddEnemy(transform.position, Colour))
+			.Subscribe(_ => Spawn())
+			.AddTo(this);
+
+		EnemiesEvents
+			.OfType<EnemiesEvent, EnemiesEvent.AllEnemiesKilled>()
+			.Delay(TimeSpan.FromSeconds(RespawnDelaySeconds))
+			.Subscribe(_ => Spawn())
 			.AddTo(this);
 	}
+
+	private void Spawn()
+		=> EnemiesCommands.AddEnemy(transform.position, Colour);
 }
